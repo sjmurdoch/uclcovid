a990253284e5f61866bb09d910d3be8d375877f5
Steven Murdoch (uclcovid) <s.murdoch@ucl.ac.uk>
2020-10-13 09:49:52 +0100

    Initial commit

---
+++ code/download.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd /home/uclcovid/uclcovid/data
+wget -qO covid-$(date '+%Y-%m-%dT%H-%M-%S').html https://www.ucl.ac.uk/coronavirus/testing-reporting-and-managing-cases/current-confirmed-cases-covid-19
