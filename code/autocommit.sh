fc04199307b89cbbc1b3aded212b488adbb28f5f
Steven Murdoch (uclcovid) <steven@github.murdoch.is>
2020-10-16 14:00:13 +0100

    Automatic commit at 2020-10-16T14-00-13

--- code/autocommit.sh
+++ code/autocommit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /home/uclcovid/uclcovid
-git pull
-git add .
-git commit -m "Automatic commit at $(date '+%Y-%m-%dT%H-%M-%S')"
-git push
+git pull -q
+git add . 
+git commit -q -m "Automatic commit at $(date '+%Y-%m-%dT%H-%M-%S')"
+git push -q
