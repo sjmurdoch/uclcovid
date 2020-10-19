80a6665ea02e52c9f877d5bededde4484b79e0e8
Steven Murdoch <s.murdoch@ucl.ac.uk>
2020-10-19 14:06:13 +0100

    Automatic commit at 2020-10-19T14-06-13

--- code/download.sh
+++ code/download.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd /home/uclcovid/uclcovid/data
-wget -qO covid-$(date '+%Y-%m-%dT%H-%M-%S').html https://www.ucl.ac.uk/coronavirus/testing-reporting-and-managing-cases/current-confirmed-cases-covid-19
+wget -qO original/covid-$(date '+%Y-%m-%dT%H-%M-%S').html https://www.ucl.ac.uk/coronavirus/testing-reporting-and-managing-cases/current-confirmed-cases-covid-19
 ../code/autocommit.sh
