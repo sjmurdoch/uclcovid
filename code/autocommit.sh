9225c338405920fdf9664a3f00bed66cc8bee5c4
Steven Murdoch <s.murdoch@ucl.ac.uk>
2020-10-27 11:24:04 +0000

    Also commit deletions

--- code/autocommit.sh
+++ code/autocommit.sh
@@ -2,6 +2,6 @@
 
 cd /home/uclcovid/uclcovid
 git pull -q
-git add . 
+git add --all . 
 git diff-index --quiet HEAD || git commit -q -m "Automatic commit at $(date '+%Y-%m-%dT%H-%M-%S')"
 git push -q
