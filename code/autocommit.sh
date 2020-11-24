21053c0b80e02d3e5dc68aba5c058470724694d2
Steven Murdoch <s.murdoch@ucl.ac.uk>
2020-11-24 10:41:19 +0000

    Pull before downloading code

--- code/autocommit.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/uclcovid/uclcovid
-git pull -q
-git add --all . 
-git diff-index --quiet HEAD || git commit -q -m "Automatic commit at $(date '+%Y-%m-%dT%H-%M-%S')"
-git push -q
