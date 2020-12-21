76697a6af9368ef5d4caffaa08d2f929ddcb8be3
Steven Murdoch <s.murdoch@ucl.ac.uk>
2020-12-21 13:52:36 +0000

    Disable data export to prevent issues in the new year

--- code/download.sh
+++ code/download.sh
@@ -5,7 +5,8 @@ git pull -q
 cd /home/uclcovid/uclcovid/data
 wget -qO original/covid-$(date '+%Y-%m-%dT%H-%M-%S').html https://www.ucl.ac.uk/coronavirus/testing-reporting-and-managing-cases/current-confirmed-cases-covid-19
 cd /home/uclcovid/uclcovid/code
-/home/uclcovid/pyvenv/uclcovid/bin/python3 exportdata.py
+## Disable over Christmas to prevent issues when it restarts
+#/home/uclcovid/pyvenv/uclcovid/bin/python3 exportdata.py
 cd /home/uclcovid/uclcovid
 git add --all .
 git diff-index --quiet HEAD || git commit -q -m "Automatic commit at $(date '+%Y-%m-%dT%H-%M-%S')"
