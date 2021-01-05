a51f0267e2f1054aaf01b9eece0a33edf7bc6af4
Steven Murdoch <s.murdoch@ucl.ac.uk>
2021-01-05 15:05:05 +0000

    Automatic commit at 2021-01-05T15-05-05

--- code/exportdata.py
+++ code/exportdata.py
@@ -137,7 +137,7 @@ def extract_df():
             else:
                 tfh.write("<h2>Data published on " + file_date.strftime("%Y-%m-%d (%A)") + "</h2>\n")
             tfh.write("<code>"+file.name+"</code>\n")
-            tfh.write(str(table))
+            tfh.write(unicode(table))
 
             pd_row = []
             pd_row.append(pd.to_datetime(data_date))
